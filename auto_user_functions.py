633293d25ac598900f9437ed8c1c8b76bb771361
Ryan McGuire <ryanmcguire@berkeley.edu>
2022-12-31 13:40:37 -0600

    clean up tiktok upload code and add file of constants for positions of clicks

---
+++ auto_user_functions.py
@@ -0,0 +1,14 @@
+import pyautogui as p
+from time import sleep
+from random import random
+
+def move(x, y, duration=.3):
+    p.move(x, y, duration=duration)
+    
+    
+def move_and_click(x, y, duration=.3):
+    move(x, y, duration=duration)
+    p.click()
+    
+def random_time(min_time, random_multiplier):
+    return min_time + random() * random_multiplier
--- tiktok_upload.py
+++ tiktok_upload.py
@@ -1,59 +1,112 @@
-def upload():
+import pyautogui as p
+from time import sleep
+from random import random
+
+from auto_user_parameters import *
+from auto_user_functions import *
+
+# def login():
+#     # Click login
+#     p.moveTo(1175, 133, duration=random() + .4)
+#     p.click()
+#     sleep(random() * 3)
+
+#     # Click login with google
+#     p.moveTo(707, 502, duration=random() + .4)
+#     p.click()
+#     sleep(random() * 3)
+
+#     # Click text field
+#     p.moveTo(707, 502, duration=random() + .4)
+#     p.click()
+#     sleep(random() * 3)
+
+#     # Type username
+#     p.typewrite("story.forge.1")
+#     sleep(random() * 3)
+
+#     # Click next
+#     p.moveTo(887, 719, duration=random() + .4)
+#     p.click()
+#     sleep(random() * 3)
+
+#     # Click text field
+#     p.moveTo(707, 502, duration=random() + .4)
+#     p.click()
+#     sleep(random() * 3)
+
+#     # Type password
+#     p.typewrite("zyxsiw-bapFo0-xignyc")
+#     sleep(random() * 3)
+
+#     # Click next
+#     p.moveTo(887, 750, duration=random() + .4)
+#     p.click()
+#     sleep(random() + 10)
+
+def select_file(filepath):
+    """ Selects file at 'filepath' in file dialog using keyboard """
     
-    # Click upload
-    # p.moveTo(1052, 272, random() + .4)
-    # p.click()
-    # time.sleep(random() + 7)
-    
-    # Click select file
-    p.moveTo(330, 673, random() + .4)
-    p.click()
-    time.sleep(1)
-    
-    # Type file path and select
     p.typewrite("\\")
-    time.sleep(3)
-    filename = '/Users/ryanmcguire/Desktop/AI_Content/generated_content/monkey_army_12222022_1846/final_video.mp4'
-    for dir in filename.split("/"):
+    sleep(3)
+    for dir in filepath.split("/"):
         p.typewrite(dir + "/")
-        time.sleep(.2)
-    time.sleep(random() + 3)
+        sleep(.2)
+        sleep(random_time(3, 1))
     p.press('enter')
-    time.sleep(random() + 3)
+    sleep(random_time(3, 1))
     p.press("enter")
     
+def type_caption(caption):
+    """ Adds caption 'caption' and adds hashtags during video upload """
+    
+    for word in caption.split(" "):
+        if "#" == word[0]:
+            p.typewrite(word)
+            sleep(random_time(2, .2))
+            p.press("enter")
+            sleep(random_time(.5, .1))
+        else:
+            p.typewrite(word + " ")
+            sleep(random_time(.5, .1))
+    p.press("backspace")
+
+def upload(filepath, caption):
+    """ From tiktok.com/upload uploads video at 'filepath', 
+        with caption 'caption' """
+    
+    # Click select file
+    x, y = TT_SELECT_FILE_BUTTON
+    move_and_click(x, y, random_time(.4, .5))
+    sleep(random_time(1, 1))
+    
+    # Type file path and select
+    select_file(filepath)
+
     # Wait for upload
-    time.sleep(32 + random() * 6)
+    sleep(random_time(45, 10))
     
     # Click caption
-    p.moveTo(809 + randint(-20, 20), 472, random() + .4)
-    p.click()
-    time.sleep(random() + 2)
+    x, y = TT_CAPTION_BOX
+    move_and_click(x, y, random_time(.4, .5))
+    sleep(random_time(1, 2))
     
     # Clear preset caption
     for _ in range(15):
         p.press("backspace")
-        time.sleep(.1)
+        sleep(random_time(.05, .1))
         
     # Type new caption
-    message = "When monkeys take over the world #tiktok #fyp #monkeys"
-    for word in message.split(" "):
-        if "#" == word[0]:
-            p.typewrite(word)
-            time.sleep(2)
-            p.press("enter")
-            time.sleep(.5)
-        else:
-            p.typewrite(word + " ")
-            time.sleep(.5)
-    p.press("backspace")
-    time.sleep(random() + 5)
+    type_caption(caption)
+    sleep(random_time(3, 2))
     
     # Scroll down
-    p.scroll(-15)
-    time.sleep(.4 + random())
+    p.scroll(TT_SCROLL_TO_UPLOAD)
+    sleep(random_time(.3, .5))
     
     # Click post
-    p.moveTo(780 + randint(-10, 10), 454, random() + .4)
-    p.click()
-    time.sleep(15)
+    x, y = TT_POST_BUTTON
+    move_and_click(x, y, random(.4, .5))
+    
+    # Wait for upload
+    sleep(25)
